556fcae04fc84492ec8859b3d52ab86843228e1d
Sanidhya Uniyal <uniyalths@gmail.com>
2024-12-08 17:29:48 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Database configuration
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
